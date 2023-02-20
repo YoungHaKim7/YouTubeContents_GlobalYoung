8c7b87d6476d19274e32aca1fb343b2f7e345a22
YoungHa Kim <ytok1108@aol.com>
2023-02-20 21:16:07 +0900

    Read & Write traits1

---
+++ 230220_Rust_tutorial_4day_5of8/23_3_Important_Traits/read_and_write/README.md
@@ -0,0 +1,13 @@
+# Result
+
+```
+$ cargo run
+
+   Compiling read_and_write v0.1.0 (/Users/globalyoung/Documents/test/test/rust/YouTubeContents_GlobalYoung/230220_Rust_tutorial_4day_5of8/23_3_Important_Traits/read_and_write)
+    Finished dev [unoptimized + debuginfo] target(s) in 0.16s
+     Running `target/debug/read_and_write`
+
+lines in slice : 3
+lines in file: 2199
+
+```
---
+++ 230220_Rust_tutorial_4day_5of8/23_3_Important_Traits/read_and_write/src/main.rs
@@ -0,0 +1,15 @@
+use std::io::{BufRead, BufReader, Read, Result};
+
+fn count_lines<R: Read>(reader: R) -> usize {
+    let buf_reader = BufReader::new(reader);
+    buf_reader.lines().count()
+}
+
+fn main() -> Result<()> {
+    let slice: &[u8] = b"foo\nbar\nbaz\n";
+    println!("lines in slice : {}", count_lines(slice));
+
+    let file = std::fs::File::open(std::env::current_exe()?)?;
+    println!("lines in file: {}", count_lines(file));
+    Ok(())
+}
