d4079f1b947446be8f55c9af9d7444aa7867932e
YoungHa Kim <ytok1108@aol.com>
2023-02-26 19:49:16 +0900

    unsafe Rust__FFI

---
+++ 230223_Rust_tutorial_4day_6of8/FFI/safe_ffi_wrapper/README.md
@@ -0,0 +1,22 @@
+# Result
+
+```
+$ cargo run
+
+    Finished dev [unoptimized + debuginfo] target(s) in 0.00s
+     Running `target/debug/safe_ffi_wrapper`
+
+files: [
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+
+```
+
+# Souce
+
+- https://doc.rust-lang.org/nomicon/ffi.html
---
+++ 230223_Rust_tutorial_4day_6of8/FFI/safe_ffi_wrapper/src/main.rs
@@ -0,0 +1,75 @@
+mod ffi {
+    use std::os::raw::{c_char, c_int, c_long, c_ulong, c_ushort};
+
+    #[repr(C)]
+    pub struct DIR {
+        _data: [u8; 0],
+        _marker: core::marker::PhantomData<(*mut u8, core::marker::PhantomPinned)>,
+    }
+
+    #[repr(C)]
+    pub struct dirent {
+        pub d_ino: c_long,
+        pub d_off: c_ulong,
+        pub d_reclen: c_ushort,
+        pub d_type: c_char,
+        pub d_name: [c_char; 256],
+    }
+
+    extern "C" {
+        pub fn opendir(s: *const c_char) -> *mut DIR;
+        pub fn readdir(s: *mut DIR) -> *const dirent;
+        pub fn closedir(s: *mut DIR) -> c_int;
+    }
+}
+
+use std::ffi::{CStr, CString, OsStr, OsString};
+use std::os::unix::ffi::OsStrExt;
+
+#[derive(Debug)]
+struct DirectoryIterator {
+    path: CString,
+    dir: *mut ffi::DIR,
+}
+
+impl DirectoryIterator {
+    fn new(path: &str) -> Result<DirectoryIterator, String> {
+        let path = CString::new(path).map_err(|err| format!("Invalid path: {err}"))?;
+
+        let dir = unsafe { ffi::opendir(path.as_ptr()) };
+        if dir.is_null() {
+            Err(format!("Could not open {:?}", path))
+        } else {
+            Ok(DirectoryIterator { path, dir })
+        }
+    }
+}
+
+impl Iterator for DirectoryIterator {
+    type Item = OsString;
+    fn next(&mut self) -> Option<OsString> {
+        let dirent = unsafe { ffi::readdir(self.dir) };
+        if dirent.is_null() {
+            return None;
+        }
+        let d_name = unsafe { CStr::from_ptr((*dirent).d_name.as_ptr()) };
+        let os_str = OsStr::from_bytes(d_name.to_bytes());
+        Some(os_str.to_owned())
+    }
+}
+
+impl Drop for DirectoryIterator {
+    fn drop(&mut self) {
+        if !self.dir.is_null() {
+            if unsafe { ffi::closedir(self.dir) } != 0 {
+                panic!("Could not close {:?}", self.path);
+            }
+        }
+    }
+}
+
+fn main() -> Result<(), String> {
+    let iter = DirectoryIterator::new(".")?;
+    println!("files: {:#?}", iter.collect::<Vec<_>>());
+    Ok(())
+}
