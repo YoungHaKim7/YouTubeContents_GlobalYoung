c9ee9c4d22b9efbe47a6122a5f6994a7744e7d0c
YoungHa Kim <ytok1108@aol.com>
2023-03-24 22:15:15 +0900

    Carriage_Return__Rust _ C _ C++

--- 230325_Carriage_Return_Rust_C_Cpp/Cpp_Lang/main.cpp
+++ 230325_Carriage_Return_Rust_C_Cpp/Cpp_Lang/main.cpp
@@ -2,14 +2,12 @@
 #include <iostream>
 #include <unistd.h>
 
-using namespace std;
-
 int main() {
   char chars[] = {'-', '\\', '|', '/'};
   unsigned int i;
 
   for (i = 0;; ++i) {
-    cout << chars[i % sizeof(chars)] << '\r';
+      std::cout << chars[i % sizeof(chars)] << '\r';
     fflush(stdout);
     usleep(200000);
   }
