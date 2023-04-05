98c9d55a1e14d9b4eed38adb3e47aa6bdb92dca6
YoungHa Kim <ytok1108@aol.com>
2023-04-05 19:25:07 +0900

    Pin to heap

---
+++ 230405_Rust_Pin_Unpin/pin_to_stack04_heap/README.md
@@ -0,0 +1,14 @@
+# Result
+
+```
+
+$ cargo run
+
+   Compiling pin_to_stack04_heap v0.1.0 (/Users/globalyoung/Documents/test/test/rust/YouTubeContents_GlobalYoung/230405_Rust_Pin_Unpin/pin_to_stack04_heap)
+    Finished dev [unoptimized + debuginfo] target(s) in 0.12s
+     Running `target/debug/pin_to_stack04_heap`
+
+a: test1, b: test1
+a: test2, b: test2
+
+```
---
+++ 230405_Rust_Pin_Unpin/pin_to_stack04_heap/src/main.rs
@@ -0,0 +1,40 @@
+use std::marker::PhantomPinned;
+use std::pin::Pin;
+
+#[derive(Debug)]
+struct Test {
+    a: String,
+    b: *const String,
+    _marker: PhantomPinned,
+}
+
+impl Test {
+    fn new(txt: &str) -> Pin<Box<Self>> {
+        let t = Test {
+            a: String::from(txt),
+            b: std::ptr::null(),
+            _marker: PhantomPinned,
+        };
+        let mut boxed = Box::pin(t);
+        let self_ptr: *const String = &boxed.a;
+        unsafe { boxed.as_mut().get_unchecked_mut().b = self_ptr };
+
+        boxed
+    }
+
+    fn a(self: Pin<&Self>) -> &str {
+        &self.get_ref().a
+    }
+
+    fn b(self: Pin<&Self>) -> &String {
+        unsafe { &*(self.b) }
+    }
+}
+
+pub fn main() {
+    let test1 = Test::new("test1");
+    let test2 = Test::new("test2");
+
+    println!("a: {}, b: {}", test1.as_ref().a(), test1.as_ref().b());
+    println!("a: {}, b: {}", test2.as_ref().a(), test2.as_ref().b());
+}
