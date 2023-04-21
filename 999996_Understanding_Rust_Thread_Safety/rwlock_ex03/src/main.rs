fe3232b9907487d49cdce727ebd2cb862791752e
YoungHa Kim <ytok1108@aol.com>
2023-04-21 22:53:19 +0900

    RwLock Ex)

---
+++ 999996_Understanding_Rust_Thread_Safety/rwlock_ex03/src/main.rs
@@ -0,0 +1,20 @@
+// https://doc.rust-lang.org/stable/std/sync/struct.RwLock.html
+use std::sync::RwLock;
+
+fn main() {
+    let lock = RwLock::new(5);
+
+    // many reader locks can be held at once
+    {
+        let r1 = lock.read().unwrap();
+        let r2 = lock.read().unwrap();
+        assert_eq!(*r1, 5);
+        assert_eq!(*r2, 5);
+    } // read locks are dropped at this point
+
+    {
+        let mut w = lock.write().unwrap();
+        *w += 1;
+        assert_eq!(*w, 6);
+    }
+}
