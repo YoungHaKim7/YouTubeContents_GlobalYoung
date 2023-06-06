e2fe6f7e3f8893d48494d7b65149ab89d856ec21
YoungHa Kim <ytok1108@aol.com>
2023-06-06 16:04:23 +0900

    Building an HTTP Server in Rust: Exploring TCP/IP, Socket Programming, and Asynchronous I/O

---
+++ 999994_Building_an_HTTP_Server_in_Rust/README.md
@@ -0,0 +1,3 @@
+# Building an HTTP Server in Rust: Exploring TCP/IP, Socket Programming, and Asynchronous I/O
+
+https://youtu.be/hzSsOV2F7-s
---
+++ 999994_Building_an_HTTP_Server_in_Rust/tcp_http_server/src/main.rs
@@ -0,0 +1,9 @@
+use std::net::SocketAddr;
+
+use std::collections::HashMap;
+
+use tokio::io::{AsyncReadExt, AsyncWriteExt, BufReader};
+
+fn main() {
+    println!("Hello, world!");
+}
