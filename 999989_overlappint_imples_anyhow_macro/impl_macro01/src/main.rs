32f8bcf966418e08d3cbe00462dece0efcd68d04
YoungHa Kim <ytok1108@aol.com>
2023-07-20 19:33:35 +0900

    Autoref-based stable specialization

--- 999989_overlappint_imples_anyhow_macro/impl_macro01/src/main.rs
+++ 999989_overlappint_imples_anyhow_macro/impl_macro01/src/main.rs
@@ -1,3 +1,27 @@
+// nightly only
+#![feature(specialization)]
+
+use std::fmt::{Display, Write};
+
+pub trait MyToString {
+    fn my_to_string(&self) -> String;
+}
+
+impl<T: Display> MyToString for T {
+    fn my_to_string(&self) -> String {
+        let mut buf = String::new();
+        buf.write_fmt(format_args!("{}", self)).unwrap();
+        buf.shrink_to_fit();
+        buf
+    }
+}
+
+impl MyToString for String {
+    fn my_to_string(&self) -> String {
+        self.clone()
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }
