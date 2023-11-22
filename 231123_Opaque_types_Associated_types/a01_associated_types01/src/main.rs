c4d81d5b962e4ed4c42698309fc85ec18033f35e
YoungHa Kim <ytok1108@aol.com>
2023-11-22 19:22:47 +0900

    Associated Types 02

--- 231123_Opaque_types_Associated_types/a01_associated_types01/src/main.rs
+++ 231123_Opaque_types_Associated_types/a01_associated_types01/src/main.rs
@@ -1,10 +1,15 @@
-trait Graph<N, E> {
-    fn has_edge(&self, _: &N, _: &N) -> bool;
-    fn edges(&self, _: &N) -> Vec<E>;
+// trait Graph<N, E> {
+trait Graph {
+    type N;
+    type E;
+
+    fn has_edge(&self, _: &Self::N, _: &Self::N) -> bool;
+    fn edges(&self, _: &Self::N) -> Vec<Self::E>;
 }
 
-/// .
-fn distance<N, E, G: Graph<N, E>>(graph: &G, start: &N, end: &N) {}
+// fn distance<N, E, G: Graph<N, E>>(graph: &G, start: &N, end: &N) {}
+
+fn distance<G: Graph>(graph: &G, start: &G::N, end: &G::N) {}
 
 fn main() {
     println!("Hello, world!");
